f6232c095ba54ecdd788bf07ab123ce621e7dbe8
spinachpasta <spinachpasta2019@gmail.com>
2021-08-28 22:18:19 +0900

    get data

--- dbaccess.js
+++ dbaccess.js
@@ -1,9 +1,9 @@
 var MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 const mongourl = process.env.MONGOURL;
+const uuidv4 = require("uuid").v4;
 
-
-const writesomething = async (req, res) => {
+module.exports.writesomething = async () => {
 	const client = await MongoClient.connect(mongourl, { useNewUrlParser: true });
 	const dbo = client.db("a8visualizer");
 	const collection = dbo.collection("request_collection");
@@ -11,5 +11,53 @@ const writesomething = async (req, res) => {
 	//const myReservation = await collection.findOne({ "_id": ObjectId(req.body.object_id) });
 	await client.close();
 };
+module.exports.taroFromHanako = async (size) => {
+	const client = await MongoClient.connect(mongourl, { useNewUrlParser: true });
+	const dbo = client.db("a8visualizer");
+	const collection = dbo.collection("taroFromHanako");
+	await collection.insertOne({ "request_uuid": uuidv4(), "timestamp": new Date(), "size": size });
+	//const myReservation = await collection.findOne({ "_id": ObjectId(req.body.object_id) });
+	await client.close();
+};
+module.exports.hanakoFromTaro = async (size) => {
+	const client = await MongoClient.connect(mongourl, { useNewUrlParser: true });
+	const dbo = client.db("a8visualizer");
+	const collection = dbo.collection("hanakoFromTaro");
+	await collection.insertOne({ "request_uuid": uuidv4(), "timestamp": new Date(), "size": size });
+	//const myReservation = await collection.findOne({ "_id": ObjectId(req.body.object_id) });
+	await client.close();
+};
+const getRecentReq = async (dbo, name) => {
+	const collection = dbo.collection(name);
+	return collection.find({}).limit(3).toArray();
+}
+module.exports.getRecent = async (collectionName) => {
+	const client = await MongoClient.connect(mongourl, { useNewUrlParser: true });
+	const dbo = client.db("a8visualizer");
+	const collection = dbo.collection(collectionName);
+	const result = await collection.find({}).limit(3).toArray();
+	await client.close();
+	return result;
+}
 
-module.exports.writesomething = writesomething;
+module.exports.getRecentHanakoAndTaro = async () => {
+	const client = await MongoClient.connect(mongourl, { useNewUrlParser: true });
+	const dbo = client.db("a8visualizer");
+	const [result, result1] =
+		await Promise.all([getRecentReq(dbo, "hanakoFromTaro"), getRecentReq(dbo, "taroFromHanako")]);
+	await client.close();
+	return { hanakoFromTaro: result, taroFromHanako: result1 };
+}
+
+module.exports.resetGame = async () => {
+	const client = await MongoClient.connect(mongourl, { useNewUrlParser: true });
+	const dbo = client.db("a8visualizer");
+	const collection = dbo.collection("taroFromHanako");
+	const collection1 = dbo.collection("hanakoFromHanako");
+	try {
+		await Promise.all([collection.drop(), collection1.drop()]);
+	} catch (e) {
+
+	}
+	await client.close();
+}
--- test/test.js
+++ test/test.js
@@ -5,8 +5,45 @@ const dbaccess = require("../dbaccess.js");
 describe('dbaccess', function () {
 	describe('writesomething', function () {
 		it('should end without errors', async function () {
-			this.timeout(10000); 
+			this.timeout(10000);
 			await dbaccess.writesomething();
+		});
+	});
+	describe('taroFromHanako', function () {
+		it('should end without errors', async function () {
+			this.timeout(10000);
+			await dbaccess.taroFromHanako();
+		});
+	});
+	describe('getRecent', function () {
+		it('should end without errors', async function () {
+			this.timeout(10000);
+			const result = await dbaccess.getRecent("taroFromHanako");
+			console.log(result);
+		});
+	});
+	describe('resetGame', function () {
+		it('should clear taroFromHanako collection', async function () {
+			this.timeout(20000);
+			await dbaccess.resetGame();
+			const result = await dbaccess.getRecent("taroFromHanako");
+			assert(result.length == 0);
+		});
+	});
+	describe('getRecentHanakoAndTaro', function () {
+		it('should clear taroFromHanako and hanakoFromTaro collection', async function () {
+			this.timeout(20000);
+			await dbaccess.taroFromHanako();
+			await dbaccess.resetGame();
+			const result = await dbaccess.getRecentHanakoAndTaro();
+			console.log(result);
+			assert(result.hanakoFromTaro.length == 0 && result.taroFromHanako == 0);
+			await Promise.all([dbaccess.taroFromHanako(),dbaccess.taroFromHanako(),dbaccess.taroFromHanako(),dbaccess.hanakoFromTaro()]);
+			const result2 = await dbaccess.getRecentHanakoAndTaro();
+			console.log(result2);
+			assert(result2.hanakoFromTaro.length !== 0);
+			assert(result2.taroFromHanako !== 0);
+
 		});
 	});
 });
