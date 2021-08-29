c7666bae36df1716a9f1db303aa8ea87bbcc3970
spinachpasta <spinachpasta2019@gmail.com>
2021-08-29 09:04:59 +0900

    boxes

--- public/visualizer.js
+++ public/visualizer.js
@@ -1,5 +1,6 @@
 const processedPackets = [];
 const boxes = [];
+let gamedata={};
 window.onload = function () {
 	async function dataloop() {
 		const result = await $.ajax({
@@ -7,6 +8,7 @@ window.onload = function () {
 			type: 'GET'
 		});
 		console.log(result);
+		gamedata=result;
 		setTimeout(dataloop, 500);
 	}
 	dataloop();
@@ -43,6 +45,7 @@ window.onload = function () {
 	const ctx = canvas[0].getContext("2d");
 	setInterval(function () {
 		ctx.clearRect(0, 0, 600, 300);
+		ctx.fillStyle="#000";
 		for (const box of boxes) {
 			box.step(0.03);
 			box.render(ctx);
@@ -55,6 +58,38 @@ window.onload = function () {
 				}
 			}
 		}
+
+		ctx.fillStyle="#eee";
+		ctx.fillRect(250,50,100,100);
+		ctx.fillStyle="#000";
+		ctx.font = "20px Arial";
+		ctx.fillText("jammer",260,100);
+
+
+		ctx.fillStyle="#efe";
+		ctx.fillRect(0,50,100,100);
+		ctx.fillStyle="#000";
+		ctx.font = "20px Arial";
+		ctx.fillText("Taro",10,100);
+
+
+
+		ctx.fillStyle="#eef";
+		ctx.fillRect(500,50,100,100);
+		ctx.fillStyle="#000";
+		ctx.font = "20px Arial";
+		ctx.fillText("Hanako",510,100);
+
+
+		ctx.font = "20px MS Pゴシック";
+		ctx.fillText("送信パケット数",10,30);
+		ctx.font = "20px Arial";
+		ctx.fillText(gamedata.sent,10,60);
+		ctx.font = "20px MS Pゴシック";
+		ctx.fillText("受信パケット数",400,30);
+		ctx.font = "20px Arial";
+		ctx.fillText(gamedata.received,400,60);
+//MS Pゴシック
 	}, 30);
 }
 function createTd(text) {
